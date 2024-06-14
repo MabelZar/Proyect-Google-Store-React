e7b088b3f9f14380adf4b88d34eaae3c594c1474
MabelZar <mabel.zar.esp@gmail.com>
2024-06-14 10:46:57 +0200

    heater done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from './NavBar/Header'; // Importa el componente Header
+
+function App() {
+  return (
+    <div >
+        <Header></Header>
+    </div>
+  );
+}
+
+export default App;
--- src/NavBar/header.jsx
+++ src/NavBar/header.jsx
@@ -1,10 +1,33 @@
-/* import React from "react";
+import './Header.css'
 
-function App(){
+import heaterImages from "../images-Header.js"
+
+function Header() {
     return(
-        <div>
-
-        </div>
+      <>
+      <div className='barra-nav'>
+          <img alt="Logo" src={heaterImages.logo} className='google-logo'/>
+          <nav className="nav">
+              <ul>
+                  <li><a href="#" className="menu">Phones</a></li>
+                  <li><a href="#"><b id="a_select">Earbuds</b></a></li>
+                  <li><a href="#" className="menu">Watches</a></li>
+                  <li><a href="#" className="menu">Smart Home</a></li>
+                  <li><a href="#" className="menu">Accessories</a></li>
+                  <li><a href="#" className="menu">Suscriptions</a></li>
+              </ul>
+          </nav>
+     
+      <div className='iconos-menu'>
+          <img id="search" alt="Search" className='icono' src={heaterImages.search}/>
+          <img alt="Help" className='icono' src={heaterImages.help}/>
+          <img alt="Cart" className='icono' src={heaterImages.cart}/>
+         
+          <img alt="Avatar" className='icono' src={heaterImages.avatar}/>
+          <img alt="menu_ham" src={heaterImages.menu} className='icono' id="ham"/>
+      </div>
+      </div>
+      </>
     )
-
-} */
+  }
+export default Header;
