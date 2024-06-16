53832e8512930c638bed859fa34867f3f1c343a9
MabelZar <mabel.zar.esp@gmail.com>
2024-06-16 02:08:28 +0200

    añadiendo cambio de main

---
+++ src/main2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Watch from './watch';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Watch />
+  </React.StrictMode>,
+
+);
