bce8b8c922afb926b97ee7f2360f1efd548c378c
MabelZar <mabel.zar.esp@gmail.com>
2024-06-17 09:35:02 +0200

    funcionalidad de rutas hecha

--- src/Earbuds.jsx
+++ src/Earbuds.jsx
@@ -1,11 +1,10 @@
 import FooterComponent from "./components/footer/component-footer.jsx";
 import MainEarbuds from "./components/Main-Earbuds/Main-Earbuds.jsx";
-// import HeaderComponent from "./components/Header/Header-component.jsx";
+
 
 const Earbuds = () => {
   return (
     <>
-      {/* <HeaderComponent></HeaderComponent> */}
       <MainEarbuds></MainEarbuds>
       <FooterComponent></FooterComponent>
       </>
