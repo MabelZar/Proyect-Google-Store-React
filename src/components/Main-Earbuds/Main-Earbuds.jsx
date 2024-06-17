aaae934fc3f6eed6688132c89e76ee273b2d1aa6
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-06-17 11:03:47 +0200

    arreglo

--- src/components/Main-Earbuds/Main-Earbuds.jsx
+++ src/components/Main-Earbuds/Main-Earbuds.jsx
@@ -4,18 +4,16 @@ import ButtonLeft from "./Button-Left";
 import ImageProduct from "./Image-Product";
 import InfoProduct from "./Info-Product";
 import InfoBox from "./Info-Box";
-
 import { products, getEarbuds } from "./data.js";
 
 function MainEarbuds() {
 
+    const earbuds = getEarbuds();
     const [currentImage, setCurrentImage] = useState(products[0].image);
-    // Estado inicial con el nombre por defecto
     const [currentName, setCurrentName] = useState(products[0].name);
-    // Estado inicial con el precio por defecto
     const [currentPrice, setCurrentPrice] = useState(products[0].price);
 
-    // Función para cambiar los datos del producto
+    
     const changeProduct = (productId) => {
         const productElement = products.find(item => item.id === productId);
         setCurrentName(productElement.name);
--- src/components/Main-Earbuds/data.js
+++ src/components/Main-Earbuds/data.js
@@ -3,42 +3,42 @@ import mainAllImages from "../../utils/images-main.js";
 const products=[
     {
         id:"P01",
-        name:"Google Pixel Buds Pro Celeste",
+        name:"Google Pixel Buds Pro",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_01_celeste
     },
     {
         id:"P02",
-        name:"Google Pixel Buds Pro Porcelana",
+        name:"Google Pixel Buds Pro ",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_02_porcelana
     },
     {
         id:"P03",
-        name:"Google Pixel Buds Pro Carbon",
+        name:"Google Pixel Buds Pro",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_03_carbon
     },
     {
         id:"P04",
-        name:"Google Pixel Buds Pro Gris",
+        name:"Google Pixel Buds Pro ",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_04_gris
     },
     {
         id:"P05",
-        name:"Google Pixel Buds Pro Verde",
+        name:"Google Pixel Buds Pro ",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_05_verde
     },
     {
         id:"P06",
-        name:"Google Pixel Buds Pro Coral",
+        name:"Google Pixel Buds Pro ",
         price: 229,
         category:"EARBUDS",
         image:mainAllImages.earbuds_06_coral
